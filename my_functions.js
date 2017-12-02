e65ad7c9ec0df004ad66d6db937e2e1de4ad7392
tfb34 <britothalia212@gmail.com>
2017-12-02 15:57:37 -0500

    Add style

--- my_functions.js
+++ my_functions.js
@@ -77,158 +77,212 @@ function subtract(a,b){
   return difference;
 }
 
-var arr=[0];
+// calculator stuff
+
+var arr=[];
 
 (function(){
    document.getElementById("0").onclick = function() { 
         console.log('0'); 
-        if(parseInt(arr[arr.length-1])){//number cont'd
-          arr[arr.length-1]+="0";
-
-        }else{
-          arr.push("0");
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="0";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
-
+        arr.push("0");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("1").onclick = function() { 
+
+(function(){
+   document.getElementById("1").onclick = function() { 
         console.log('1'); 
-        if(parseInt(arr[arr.length-1])){//number cont'd
-          arr[arr.length-1]+="1";
-
-        }else{
-          arr.push("1");
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="1";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
-
+        arr.push("1");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("2").onclick = function() { 
+(function(){
+   document.getElementById("2").onclick = function() { 
         console.log('2'); 
-        if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="2";
-        }else{
-          arr.push("2");
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="2";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
+        arr.push("2");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("3").onclick = function() { 
-        console.log('3');
-        if(parseInt(arr[arr.length-1])){
-          arr[length-1]+="3";
-        }else{
-          arr.push("3");
+(function(){
+   document.getElementById("3").onclick = function() { 
+        console.log('3'); 
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="3";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
+        arr.push("3");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("4").onclick = function() { 
+
+(function(){
+   document.getElementById("4").onclick = function() { 
         console.log('4'); 
-        if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="4";
-        }else{
-          arr.push("4");
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="4";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
+        arr.push("4");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("5").onclick = function() { 
+(function(){
+   document.getElementById("5").onclick = function() { 
         console.log('5'); 
-      if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="5";
-        }else{
-          arr.push("5");
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="5";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
         }
+        arr.push("5");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("6").onclick = function() { 
+
+(function(){
+   document.getElementById("6").onclick = function() { 
         console.log('6'); 
-      if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="6";
-      }else{
-          arr.push("6");
-      }
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="6";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
+        }
+        arr.push("6");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("7").onclick = function() { 
+
+(function(){
+   document.getElementById("7").onclick = function() { 
         console.log('7'); 
-      if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="7";
-      }else{
-          arr.push("7");
-      }
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="7";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
+        }
+        arr.push("7");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("8").onclick = function() { 
+(function(){
+   document.getElementById("8").onclick = function() { 
         console.log('8'); 
-      if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="8";
-      }else{
-          arr.push("8");
-      }
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="8";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
+        }
+        arr.push("8");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
-(function() {
-    document.getElementById("9").onclick = function() { 
+(function(){
+   document.getElementById("9").onclick = function() { 
         console.log('9'); 
-      if(parseInt(arr[arr.length-1])){
-          arr[arr.length-1]+="9";
-      }else{
-          arr.push("9");
-      }
+        if(arr.length !== 0){//not empty
+          if(parseFloat(arr[arr.length-1]) || arr[arr.length-1].includes(".")){//5.5 or 0.
+            arr[arr.length-1]+="9";
+            document.getElementById("window").innerHTML = arr.join('');
+            return;
+          }
+        }
+        arr.push("9");
+        document.getElementById("window").innerHTML = arr.join('');
     };
 })();
+
+
 
 (function() {
     document.getElementById("+").onclick = function() { 
         console.log('+'); 
-      if(arr.length === 1){
-        //do nothing
-      }else if(parseInt(arr[arr.length-1])){
+      if(arr.length > 0){
+        let last = arr[arr.length-1];
+        if(!isMathOp(last)){
           arr.push("+");
-      }else{//change operation
-          arr[arr.length-1] = "+";
+        }else{
+          arr[arr.length-1]="+";
+        }
       }
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
 (function() {
     document.getElementById("-").onclick = function() { 
         console.log('-'); 
-      if(arr.length === 1){
-        //do nothing
-      }else if(parseInt(arr[arr.length-1])){
+      if(arr.length > 0){
+        let last = arr[arr.length-1];
+        if(!isMathOp(last)){
           arr.push("-");
-      }else{//change operation
-          arr[arr.length-1] = "-";
+        }else{
+          arr[arr.length-1]="-";
+        }
+        document.getElementById("window").innerHTML = arr.join('');
+        return;
       }
+      //allow user to enter this value first
+      arr.push("-");
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
 (function() {
     document.getElementById("/").onclick = function() { 
         console.log('/'); 
-      if(arr.length === 1){
-        //do nothing
-      }else if(parseInt(arr[arr.length-1])){
+      if(arr.length > 0){
+        let last = arr[arr.length-1];
+        if(!isMathOp(last)){
           arr.push("/");
-      }else{//change operation
-          arr[arr.length-1] = "/";
+        }else{
+          arr[arr.length-1]="/";
+        }
       }
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
@@ -236,45 +290,68 @@ var arr=[0];
 (function() {
     document.getElementById("x").onclick = function() { 
         console.log('x'); 
-      if(arr.length === 1){
-        //do nothing
-      }else if(parseInt(arr[arr.length-1])){
+      if(arr.length > 0){
+        let last = arr[arr.length-1];
+        if(!isMathOp(last)){
           arr.push("*");
-      }else{//change operation
-          arr[arr.length-1] = "*";
+        }else{
+          arr[arr.length-1]="*";
+        }
       }
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
+
+function isMathOp(x){
+  if(x==='+' || x==='-' || x==='/' || x==='*'){
+    return true;
+  }
+  return false;
+}
 
 (function() {
     document.getElementById("=").onclick = function() { 
         console.log('='); 
-      if (parseInt(arr[arr.length-1]) || arr[arr.length-1] === "0") {
-        console.log(eval(arr.slice(1).join('')));
-      }else{//last element is a math operation
-        console.log(eval(arr.slice(1,arr.length-1).join('')));
+      let result = null;
+      if(arr.length === 0){
+        console.log(eval(0));
+        result = eval(0);
       }
-
-      arr=[0];
+      else if (isMathOp(arr[arr.length-1])) {
+        console.log(eval(arr.slice(0,arr.length-1).join('')));
+        result = eval(arr.slice(0,arr.length-1).join(''));
+      }else{//last element is a math operation
+        console.log(eval(arr.join('')));
+        result = eval(arr.join(''));
+      }
+      document.getElementById("window").innerHTML = arr.join('') + "=" + result;
+      arr=[];
     };
 })();
 
 (function() {
     document.getElementById("clear").onclick = function() { 
         console.log('clear'); 
-      arr = [0];
+      arr = [];
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
 
 (function() {
     document.getElementById(".").onclick = function() { 
         console.log('.'); 
-      if(parseInt(arr[arr.length-1]) && !arr[arr.length-1].includes(".")){
-         arr[arr.length-1]+=".";
-      }else if(arr[arr.length-1].includes(".")){
-          //do nothing
-      }else{//
-        arr.push(".");
+
+      if(arr.length !== 0){
+        let last = arr[arr.length-1];
+        if(isMathOp(last)){
+          arr.push("0.");
+        }
+        else if(  !last.includes(".") ){
+          arr[arr.length-1]+=".";
+        }
+      }else{
+         arr.push("0.");
       }
+      document.getElementById("window").innerHTML = arr.join('');
     };
 })();
